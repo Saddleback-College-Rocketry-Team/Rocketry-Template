097d387e7220bc30e64d10b13d41be0d9758b53f
Ansuperope <aspenfbn@yahoo.com>
2026-03-06 19:04:26 -0800

    testing commands

---
+++ test/test_native/test_main.cpp
@@ -0,0 +1,17 @@
+#include <unity.h>
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_basic_math(void) {
+  TEST_ASSERT_EQUAL_INT(5, 2 + 3);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_basic_math);
+  return UNITY_END();
+}
